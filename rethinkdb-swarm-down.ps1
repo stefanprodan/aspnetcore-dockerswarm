10a27011988dcce843ccbacf21500efc0370d45a
Stefan Prodan <stefan.prodan@gmail.com>
2016-08-12 00:21:32 +0300

    rethinkdb cluster ops scripts

---
+++ rethinkdb-swarm-down.ps1
@@ -0,0 +1,15 @@
+﻿# remove RethinkDB cluster
+docker service rm rdb-proxy
+docker service rm rdb-primary
+docker service rm rdb-secondary
+
+Start-Sleep -s 5
+
+# remove all containers
+docker rm -f $(docker ps -a -q)
+
+# remove all stopped containers
+# docker rm -v $(docker ps -a -q -f status=exited)
+
+# remove all unused images
+# docker rmi $(docker images -q -f dangling=true)
--- rethinkdb-swarm-up.ps1
+++ rethinkdb-swarm-up.ps1
@@ -1,8 +1,12 @@
 ﻿# create and start rethinkdb primary 
-docker service create --name rdb-primary --network backend-net rethinkdb:latest rethinkdb --bind all --no-http-admin 
+docker service create --name rdb-primary --network backend-net rethinkdb:latest rethinkdb --bind all --no-http-admin
 
-# create and start rethinkdb replica x3 
-docker service create --name rdb-replica --replicas 3 --network backend-net rethinkdb:latest rethinkdb --bind all --no-http-admin --join rdb-primary
+Start-Sleep -s 5
+
+# create and start rethinkdb secondary
+docker service create --name rdb-secondary --network backend-net rethinkdb:latest rethinkdb --bind all --no-http-admin --join rdb-primary
+
+Start-Sleep -s 5
 
 # create and start rethinkdb proxy 
 docker service create --name rdb-proxy --network backend-net --publish 8080:8080 rethinkdb:latest rethinkdb proxy --bind all --join rdb-primary
