623f762f6f7d7badbfeeda4c5f4c791f9c4d3fff
Stefan Prodan <stefan.prodan@gmail.com>
2016-08-12 00:22:07 +0300

    load test for RethinkDB cluster

---
+++ swarm-loadtest.ps1
@@ -0,0 +1,15 @@
+﻿workflow loadtest{
+    Param($Iterations)
+
+    $array = 1..$Iterations
+
+    foreach -Parallel -ThrottleLimit 10 ($i in $array){
+        Invoke-RestMethod http://10.0.75.2:5000/api/token
+    }
+}
+
+# run load test
+loadtest 1000
+
+# display issuers load 
+# Invoke-RestMethod http://10.0.75.2:5000/api/issuer
