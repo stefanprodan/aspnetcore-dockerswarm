3bf4a1b93aaa70933f322773d83deb4868600e70
Stefan Prodan <stefan.prodan@gmail.com>
2016-08-16 19:05:12 +0300

    scale primary x2

--- rethinkdb-swarm-up.ps1
+++ rethinkdb-swarm-up.ps1
@@ -8,12 +8,7 @@ docker service create --name rdb-secondary --network backend-net --replicas 1 re
 
 Start-Sleep -s 5
 
-# create and start rethinkdb proxy 
-docker service create --name rdb-proxy --network backend-net --publish 8080:8080 --publish 28015:28015 rethinkdb:latest rethinkdb proxy --bind all --join rdb-primary
-
-Start-Sleep -s 5
-
-# up 3 nodes to enable automatic failover
+# up 3 nodes (primary + 2 secondary) to enable automatic failover
 docker service scale rdb-secondary=2
 
 Start-Sleep -s 5
@@ -23,3 +18,13 @@ docker service rm rdb-primary
 
 # recreate primary with --join flag
 docker service create --name rdb-primary --network backend-net rethinkdb:latest rethinkdb --bind all --no-http-admin --join rdb-secondary
+
+Start-Sleep -s 5
+
+# start 2 rdb-primary instances
+docker service scale rdb-primary=2
+
+Start-Sleep -s 5
+
+# create and start rethinkdb proxy 
+docker service create --name rdb-proxy --network backend-net --publish 8080:8080 --publish 28015:28015 rethinkdb:latest rethinkdb proxy --bind all --join rdb-primary
