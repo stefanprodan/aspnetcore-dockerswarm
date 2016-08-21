6e78c5bd2c406fcd76dcb0ef36ec308cca364f0d
Stefan Prodan <stefan.prodan@gmail.com>
2016-08-21 14:15:42 +0300

    refactoring js

--- src/LogWatcher/wwwroot/js/site.js
+++ src/LogWatcher/wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿// Write your Javascript code.
+﻿function showAlert(alert, message) {
+    alert.find("p").text(message);
+    alert.show();
+}
+
+$(function () {
+
+    var alert = $('#alert');
+
+    // enable SignalR logging
+    $.connection.hub.logging = true;
+
+    // alert on slow connection
+    $.connection.hub.connectionSlow(function () {
+        showAlert(alert, 'We are currently experiencing difficulties with the SignalR connection');
+    });
+
+    // alert on connection error
+    $.connection.hub.error(function (error) {
+        showAlert(alert, 'SignalR error: ' + error);
+    });
+
+    // alert on reconnected
+    $.connection.hub.reconnected(function () {
+        showAlert(alert, 'Reconnected to SignalR hub, transport ' + $.connection.hub.transport.name);
+    });
+});
