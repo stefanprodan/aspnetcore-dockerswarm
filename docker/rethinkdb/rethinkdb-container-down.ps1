80b020f1daf010e010a0704805746ac7fbfe8470
Stefan Prodan <stefan.prodan@gmail.com>
2016-08-30 11:59:50 +0300

    backend services scripts moved in folders

---
+++ docker/rethinkdb/rethinkdb-container-down.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = "Stop"
+
+# stop container if running 
+docker stop rethinkdev
+
+# remove container
+docker rm rethinkdev
---
+++ docker/swarm-setup.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop"
+
+# initialize swarm
+if(!(docker info).contains("Swarm: active")){
+	docker swarm init
+}
+
+# create network
+$network = "backend-net"
+if(!(docker network ls --filter name=$network -q)){
+	docker network create --driver overlay $network
+}
