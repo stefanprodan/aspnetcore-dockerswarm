55a94c61dc76d4e63de6f4f47fc8efc00003b5a0
Stefan Prodan <stefan.prodan@gmail.com>
2016-09-01 16:08:40 +0300

    swarm cleanup script

---
+++ docker/swarm-cleanup.ps1
@@ -0,0 +1,12 @@
+﻿# Remove all stopped containers
+docker rm $(docker ps -a -q -f "status=exited")
+
+Start-Sleep 5
+
+# Remove untagged images
+docker rmi $(docker images -q -f "dangling=true")
+
+Start-Sleep 5
+
+# Remove orphaned volumes
+docker volume rm $(docker volume ls -q -f "dangling=true")
